ce9978c5d6ec8d7992d5d40beb37d9ea19921656
reawei <reawei@vip.qq.com>
2018-07-06 15:50:03 +0800

    first commit

---
+++ src/common/menu.js
@@ -0,0 +1,23 @@
+import { isUrl } from '../utils/utils';
+
+const menuData = JSON.parse(localStorage.getItem(`menus`));
+
+function formatter(data, parentPath = '/', parentAuthority) {
+  return data.map(item => {
+    let { path } = item;
+    if (!isUrl(path)) {
+      path = parentPath + item.path;
+    }
+    const result = {
+      ...item,
+      path,
+      authority: item.authority || parentAuthority,
+    };
+    if (item.children) {
+      result.children = formatter(item.children, `${parentPath}${item.path}/`, item.authority);
+    }
+    return result;
+  });
+}
+
+export const getMenuData = () => formatter(menuData);
---
+++ src/models/global.js
@@ -0,0 +1,75 @@
+import { queryNotices, queryMenu } from '../services/api';
+
+export default {
+  namespace: 'global',
+
+  state: {
+    collapsed: false,
+    notices: [],
+  },
+
+  effects: {
+    *fetchNotices(_, { call, put }) {
+      const data = yield call(queryNotices);
+      yield put({
+        type: 'saveNotices',
+        payload: data,
+      });
+      yield put({
+        type: 'user/changeNotifyCount',
+        payload: data.length,
+      });
+    },
+    *fetchMenus(_, { call }) {
+      const data = yield call(queryMenu);
+      localStorage.setItem('menus', JSON.stringify(data));
+    },
+    *clearNotices({ payload }, { put, select }) {
+      yield put({
+        type: 'saveClearedNotices',
+        payload,
+      });
+      const count = yield select(state => state.global.notices.length);
+      yield put({
+        type: 'user/changeNotifyCount',
+        payload: count,
+      });
+    },
+  },
+
+  reducers: {
+    changeLayoutCollapsed(state, { payload }) {
+      return {
+        ...state,
+        collapsed: payload,
+      };
+    },
+    saveNotices(state, { payload }) {
+      return {
+        ...state,
+        notices: payload,
+      };
+    },
+    saveClearedNotices(state, { payload }) {
+      return {
+        ...state,
+        notices: state.notices.filter(item => item.type !== payload),
+      };
+    },
+  },
+
+  subscriptions: {
+    setup({ dispatch, history }) {
+      return history.listen(({ pathname, search }) => {
+        if(!localStorage.getItem(`menus`)){
+          dispatch({
+            type: 'fetchMenus',
+          })
+        }
+        if (typeof window.ga !== 'undefined') {
+          window.ga('send', 'pageview', pathname + search);
+        }
+      });
+    },
+  },
+};
